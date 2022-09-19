2c763ec14db44eb717bf1e95d588278a899baeab
Robin Hextall-Schjelderup <robinh-s@hotmail.com>
2022-09-19 20:07:32 +0200

    Feat: Added profile component with logout button, and navigation to translate page

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,5 +1,47 @@
-const Profile = () => {
-  return <div>Profile</div>
+import { useState } from "react"
+import { UserContext } from "../UserContextProvider"
+import { useContext } from "react"
+import { useNavigate } from "react-router-dom"
+import { getTranslationsByUserId } from "../../common/util/API"
+
+
+
+function Profile() {
+  const userContext = useContext(UserContext)
+  const user = userContext.getUser()
+  const navigate = useNavigate()
+  const listOfTranslations = getTranslationsByUserId(user.id)
+
+
+  function handleLogout() {
+    console.log("Logging out")
+    //Logging out and setting users authentication to false
+    user.isAuthenticated = false
+    //navigating to login page
+    navigate("/login")
+    }
+  
+  //Going to translate page
+  function handleToSign() {
+  console.log("Going to translate page")
+  //navigating to login page
+  navigate("/translate")
+  }
+
+  return (
+    <>
+        <p>User Data:</p>
+        {user.username ? <p>{user.username}</p> : <p>No user logged in</p>}
+        <button id = "logoutBut" onClick={handleLogout}>Sign out</button>
+        <button id = "toSignBut" onClick={handleToSign}>Translate page</button>
+        <p id = "translation-list">Translations:</p>
+        <ul></ul>
+        
+    </>
+)
+
+
 }
+
 
 export default Profile
